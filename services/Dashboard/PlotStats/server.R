1cd1840fcdf85cc3edbfe89b9130bf906716002b
Nasim <nasimgh@users.noreply.github.com>
2015-06-30 12:25:01 -0600

    new changes have been applied
    Whats new in this version:
    - Usernames are sorted alphabetically 
    - Bars in x-axis are sorted based on the number of plots each user has uploaded in that range
    - Date range picker has been added instead of selecting months. minimum date is set to the oldest plot and maximum date is set to the newest plot (default values). Records are aggregated based on the month.
    - Slider input has been added to filter the number of plots per user in each month. min/max is set to min/max of number of plots that users uploaded.

--- services/Dashboard/PlotStats/server.R
+++ services/Dashboard/PlotStats/server.R
@@ -1,6 +1,6 @@
 ## Author: Nasim Gh. 
 ## email: n-ghazan@nmsu.edu
-## 6/24/2015
+## 6/30/2015
 
 
 # server.R
@@ -17,7 +17,7 @@ shinyServer(function(input, output,session) {
     checkboxGroupInput("checkGroup", 
                        label = h5("Usernames:"), 
                        choices = userNames ,
-                       selected = NULL)#c("dwkimiti@gmail.com","corwaal@gmail.com"))
+                       selected = NULL)
     
   })
   
@@ -42,62 +42,19 @@ shinyServer(function(input, output,session) {
   
 
   ###########################
-  
-  output$MonthList <- renderUI({  
-    
-    checkboxGroupInput("checkMonthGroup", 
-                       label = h5("Months:"), 
-                       choices = months ,
-                       selected = NULL)
-    
-  })
-  
-  observe({
-    if (input$selectAllMonth){
-      
-      updateCheckboxGroupInput( session, "checkMonthGroup", 
-                                choices = months ,
-                                selected = months) 
-    }
-  })
-  
-  observe({
-    if(input$deselectAllMonth)
-    {
-      updateCheckboxGroupInput( session, "checkMonthGroup", 
-                                choices = months ,
-                                selected = NULL  ) 
-    }
-    
-  })
-  
-
-
-  ###########################
 
   output$chart1 <- renderGvis({
   
     shiny::validate(need(input$checkGroup , 'Check at least one userName!' ))    
-    shiny::validate(need(input$checkMonthGroup , 'Check at least one date!' ))  
+    #shiny::validate(need(input$checkMonthGroup , 'Check at least one date!' )) 
+    shiny::validate(need(input$dates[2] > input$dates[1], "end date should be after start date"))
 
-    UserData<-getUserData(input$checkGroup,input$checkMonthGroup)
+    UserData<-getUserData(input$checkGroup,input$dates,input$count)
     
     shiny::validate(need(UserData , 'No data to display!' ))  
-    #UserData<-getTransposeData(input$checkGroup)
+
     
     gvisColumnChart(UserData)
-#       gvisAnnotationChart(UserData, datevar="time",
-#                           numvar="x", idvar="user",
-#                           titlevar="", annotationvar="user",
-#                           chartid="AnnotationChart",
-#                           options=list(
-#                             width=1000, height=550,
-#                             displayAnnotationsFilter = TRUE,
-#                              displayExactValues =TRUE,
-# #                             scaleColumns='[0]',
-#                             scaleType='allmaximized',
-#                             fill=10, displayExactValues=TRUE)
-#       )    
 
   })
 }) 
