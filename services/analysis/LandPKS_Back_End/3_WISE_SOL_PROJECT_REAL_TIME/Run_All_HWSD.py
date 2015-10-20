af02f388cd13b30a80fa719914fc06b6ad85f0f8
Thanh Nguyen <thanhnh2911@gmail.com>
2015-10-20 10:55:43 -0600

    Update Run_All_HWSD.py

--- services/analysis/LandPKS_Back_End/3_WISE_SOL_PROJECT_REAL_TIME/Run_All_HWSD.py
+++ services/analysis/LandPKS_Back_End/3_WISE_SOL_PROJECT_REAL_TIME/Run_All_HWSD.py
@@ -8,7 +8,7 @@ if (len(sys.argv) < 6):
     #os.system("python Step_1_preprocessing_HWSD_SPAW.py -in C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/map_location.csv -ou complete_map_location.csv -tif D:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/Model_Application/Core/LPKS_SPAW.xlsx")
     #os.system("python Step_1_preprocessing_HWSD_SPAW_real_time.py -x 15.36858 -y 1.2895 -tif D:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/Model_Application/Core/LPKS_SPAW.xlsx")
     #os.system("python Step_1_preprocessing_HWSD_ROSETTA_real_time.py -x 36.88141147 -y 0.3382613 -tif D:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/Rosetta_Model_Application/Rosetta")
-    os.system("python Step_1_preprocessing_HWSD_ROSETTA.py -in C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/map_location.csv -ou complete_map_location.csv -tif D:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/Rosetta_Model_Application/Rosetta")
+    os.system("python Step_1_preprocessing_HWSD_ROSETTA.py -in C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/map_location.csv -ou complete_map_location.csv -tif E:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT/Rosetta_Model_Application/Rosetta")
 else:
     X_Coor = 0.00
     Y_Coor = 0.00
@@ -37,5 +37,5 @@ else:
     else:
         sys.exit("====[Error] : Error in Folder")
      
-    os.system("python Step_1_preprocessing_HWSD_ROSETTA_real_time.py -x %f -y %f -tif D:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT_REAL_TIME/Rosetta_Model_Application/Rosetta -ID %s" %(X_Coor,Y_Coor,ID))#Access GDAL to    
+    os.system("python Step_1_preprocessing_HWSD_ROSETTA_real_time.py -x %f -y %f -tif E:/ThanhNguyen_Working/Python_APEX/TIF_FILE_COLLECTION/ -model C:/xampp/htdocs/APEX/Python_APEX/3_WISE_SOL_PROJECT_REAL_TIME/Rosetta_Model_Application/Rosetta -ID %s" %(X_Coor,Y_Coor,ID))#Access GDAL to    
     
